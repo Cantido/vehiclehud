e8542c39bc32a2a339fd5d2025594d651d2f26d8
Robert Richter <robert.c.richter@gmail.com>
2013-05-02 21:31:16 -0500

    Add documentation for functions

--- main.c
+++ main.c
@@ -24,26 +24,6 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
-    /*
-     * Configuring serial communication with the ELM327:
-     *
-     *  Baud rate: 9600
-     *  Data Bits: 8 bits
-     *  Parity: No parity
-     *  Stop Bits: 1
-     *  Newline Character: \r
-     */
-     
-     /* ---------------------------------------------------------------------
-     * Initializing the ELM327:
-     *  1. Reset the chip by sending it the string "ATZ"
-     *  2. (optional) verify that the chip has sent "ELM327 v1.4b"
-     *  3. Disable command echoing from the chip with "ATE0"
-     *  4. Ask the chip to find a protocol with "ATSP0"
-     *
-     * Note: All strings must be terminated by a carriage return.
-     * ---------------------------------------------------------------------*/
-
 #define TTYPREFIX "/dev/tty"
 #define BAUDRATE            B9600
 #define DATABITS                1
@@ -67,7 +47,27 @@ int main(int argc, char** argv) {
     return 0;
 }
 
+/*
+ * Applies settings to the ELM327 and makes sure that it is ready to
+ *  communicate.
+ *
+ * Arguments: a file descriptor to a connection with the ELM
+ * Preconditions: a connection with the ELM has been established
+ * Postconditions: the ELM is configured and is ready to use
+ * Returns: Nothing
+ *
+ */
+
 void obd_init(const int obd_fd) {
+     /* ---------------------------------------------------------------------
+     * Initializing the ELM327:
+     *  1. Reset the chip by sending it the string "ATZ"
+     *  2. (optional) verify that the chip has sent "ELM327 v1.4b"
+     *  3. Disable command echoing from the chip with "ATE0"
+     *  4. Ask the chip to find a protocol with "ATSP0"
+     *
+     * Note: All strings must be terminated by a carriage return.
+     * ---------------------------------------------------------------------*/
     ssize_t charsread;
     
     write(obd_fd, "ATZ\r", 4);
@@ -80,11 +80,28 @@ void obd_init(const int obd_fd) {
     return;
 }
 
+/*
+ * Attempts to open up & configure a file descriptor for a serial connection to
+ *  the ELM327. If there is any sort of failure, the program exits.
+ *
+ * Preconditions: None
+ * Postconditions: A serial port /dev/ttySX is reconfigured
+ * Returns: A file descriptor to the serial port
+ *
+ */
+
 int serial_connect() {
     struct termios obdcfg;
     int obd_fd = 0;
     char portname[11];
     int conn_success_flag = 0;
+    
+    /*
+     * Step 1: Open a file descriptor to /dev/ttySX
+     *
+     *  Method: Iterate through /dev/ttyS0 through /dev/ttyS9,
+     *    and see if any work.
+     */
     
     printf("Attempting to connect to a serial port...\n");
     
@@ -105,14 +122,28 @@ int serial_connect() {
         exit(EXIT_FAILURE);
     }
     
-    obdcfg.c_iflag |= ( ICRNL );  // Translate CR to NL on input
+    /*
+     * Step 2: Set termios flags
+     *
+     *  Input flags:   ICRNL: Translate CR to NL on input
+     *  Output flags:  none
+     *  Control flags: CS8: Set character size to 8 bits
+     *
+     *  Input and Output baud rate set to BAUDRATE
+     */
+    
+    obdcfg.c_iflag |= ( ICRNL );
     //obdcfg.c_oflag |= ();
-    obdcfg.c_cflag |= ( CS8 );    // Character size: 8 bits
+    obdcfg.c_cflag |= ( CS8 );
     
     if(cfsetispeed(&obdcfg, BAUDRATE) < 0 || cfsetospeed(&obdcfg, BAUDRATE) < 0) {
         perror("Error setting baud rate");
         exit(EXIT_FAILURE);
     }
+    
+    /*
+     * Step 3: Apply termios attributes to the file descriptor
+     */
     
     printf("Setting serial attributes...");
     fflush(stdout);
@@ -124,6 +155,10 @@ int serial_connect() {
         printf("Success!\n");
     }
     
+    /*
+     * Done!
+     */
+     
     printf("Serial connection established\n");
     
     return obd_fd;
