616a03ff58998c4d1425514e2df04a4fcb3f7731
Aaron Hall <ventscanner5@yahoo.com>
2013-06-24 19:40:12 -0500

    Added sample C# project for OBD communication.

---
+++ C#/OBD Test/OBD Test/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace OBD_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnInit = new System.Windows.Forms.Button();
+            this.txtSpeed = new System.Windows.Forms.TextBox();
+            this.txtRPM = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.RPM = new System.Windows.Forms.Label();
+            this.serialOBD = new System.IO.Ports.SerialPort(this.components);
+            this.cmbPorts = new System.Windows.Forms.ComboBox();
+            this.btnComm = new System.Windows.Forms.Button();
+            this.rtbPrgStat = new System.Windows.Forms.RichTextBox();
+            this.rtbOBDMsg = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.barRPM = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnInit
+            // 
+            this.btnInit.Location = new System.Drawing.Point(12, 87);
+            this.btnInit.Name = "btnInit";
+            this.btnInit.Size = new System.Drawing.Size(75, 23);
+            this.btnInit.TabIndex = 0;
+            this.btnInit.Text = "Initialize";
+            this.btnInit.UseVisualStyleBackColor = true;
+            this.btnInit.Click += new System.EventHandler(this.btnInit_Click);
+            // 
+            // txtSpeed
+            // 
+            this.txtSpeed.Location = new System.Drawing.Point(243, 108);
+            this.txtSpeed.Name = "txtSpeed";
+            this.txtSpeed.Size = new System.Drawing.Size(100, 20);
+            this.txtSpeed.TabIndex = 1;
+            // 
+            // txtRPM
+            // 
+            this.txtRPM.Location = new System.Drawing.Point(243, 134);
+            this.txtRPM.Name = "txtRPM";
+            this.txtRPM.Size = new System.Drawing.Size(100, 20);
+            this.txtRPM.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(196, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Speed:";
+            // 
+            // RPM
+            // 
+            this.RPM.AutoSize = true;
+            this.RPM.Location = new System.Drawing.Point(203, 134);
+            this.RPM.Name = "RPM";
+            this.RPM.Size = new System.Drawing.Size(34, 13);
+            this.RPM.TabIndex = 4;
+            this.RPM.Text = "RPM:";
+            // 
+            // cmbPorts
+            // 
+            this.cmbPorts.FormattingEnabled = true;
+            this.cmbPorts.Location = new System.Drawing.Point(12, 60);
+            this.cmbPorts.Name = "cmbPorts";
+            this.cmbPorts.Size = new System.Drawing.Size(121, 21);
+            this.cmbPorts.TabIndex = 5;
+            // 
+            // btnComm
+            // 
+            this.btnComm.Location = new System.Drawing.Point(243, 160);
+            this.btnComm.Name = "btnComm";
+            this.btnComm.Size = new System.Drawing.Size(75, 23);
+            this.btnComm.TabIndex = 6;
+            this.btnComm.Text = "Get Data";
+            this.btnComm.UseVisualStyleBackColor = true;
+            this.btnComm.Click += new System.EventHandler(this.btnComm_Click);
+            // 
+            // rtbPrgStat
+            // 
+            this.rtbPrgStat.Location = new System.Drawing.Point(28, 279);
+            this.rtbPrgStat.Name = "rtbPrgStat";
+            this.rtbPrgStat.Size = new System.Drawing.Size(172, 147);
+            this.rtbPrgStat.TabIndex = 7;
+            this.rtbPrgStat.Text = "";
+            // 
+            // rtbOBDMsg
+            // 
+            this.rtbOBDMsg.Location = new System.Drawing.Point(206, 279);
+            this.rtbOBDMsg.Name = "rtbOBDMsg";
+            this.rtbOBDMsg.Size = new System.Drawing.Size(193, 147);
+            this.rtbOBDMsg.TabIndex = 8;
+            this.rtbOBDMsg.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 263);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Program Status";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(203, 263);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "OBD Responses";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Available COM Ports";
+            // 
+            // barRPM
+            // 
+            this.barRPM.Location = new System.Drawing.Point(243, 79);
+            this.barRPM.Name = "barRPM";
+            this.barRPM.Size = new System.Drawing.Size(100, 23);
+            this.barRPM.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 438);
+            this.Controls.Add(this.barRPM);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rtbOBDMsg);
+            this.Controls.Add(this.rtbPrgStat);
+            this.Controls.Add(this.btnComm);
+            this.Controls.Add(this.cmbPorts);
+            this.Controls.Add(this.RPM);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRPM);
+            this.Controls.Add(this.txtSpeed);
+            this.Controls.Add(this.btnInit);
+            this.Name = "Form1";
+            this.Text = "OBD Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInit;
+        private System.Windows.Forms.TextBox txtSpeed;
+        private System.Windows.Forms.TextBox txtRPM;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label RPM;
+        private System.IO.Ports.SerialPort serialOBD;
+        private System.Windows.Forms.ComboBox cmbPorts;
+        private System.Windows.Forms.Button btnComm;
+        private System.Windows.Forms.RichTextBox rtbPrgStat;
+        private System.Windows.Forms.RichTextBox rtbOBDMsg;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar barRPM;
+    }
+}
+
---
+++ C#/OBD Test/OBD Test/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace OBD_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {   
+            //define stuff we need
+            string[] ports = SerialPort.GetPortNames();
+
+            InitializeComponent();
+            btnComm.Enabled = false;
+
+            foreach (string port in ports)
+                cmbPorts.Items.Add(port);
+
+        }
+
+        private void btnInit_Click(object sender, EventArgs e)
+        {
+            new Thread(OBDInit).Start();
+        }
+
+        private void btnComm_Click(object sender, EventArgs e)
+        {
+            new Thread(OBDComm2).Start();
+        }
+
+        public void UpdateTextBox(TextBox box, string value)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<TextBox, string>(UpdateTextBox), new object[] {box, value });
+                return;
+            }
+            box.Text = value;
+        }
+
+        int GetSpeed()
+        {
+            int i = 0;
+            char inChar = ' ';
+            char[] data = new char[11];
+
+            //get rid of any junk in the buffer
+            serialOBD.DiscardInBuffer();
+
+            //ask for speed
+            serialOBD.Write("010D\r\n");
+
+            //wait until car is done responding
+            //while (serialOBD.BytesToRead < 11) Thread.Sleep(25);
+
+            //read in each character
+            do
+            {
+                inChar = (char)serialOBD.ReadChar();
+                if ((inChar != '\r') && (inChar != ' ') && (inChar != '>'))
+                {
+                    data[i] = inChar;
+                    ++i;
+                }
+            } while (inChar != '\r');
+
+            //translate data
+            return Convert.ToInt16(new string(data, 4, 2), 16);
+        }
+
+        int GetRPM()
+        {
+            int i = 0;
+            char inChar = ' ';
+            char[] data = new char[14];
+            //get rid of any junk in the buffer
+            serialOBD.DiscardInBuffer();
+
+            //ask for RPM
+            serialOBD.Write("010C\r\n");
+
+            //wait until car is done responding
+            while (serialOBD.BytesToRead < 14) Thread.Sleep(25);
+
+            //read in each character
+            do
+            {
+                inChar = (char)serialOBD.ReadChar();
+                if ((inChar != '\r') && (inChar != ' ') && (inChar != '>'))
+                {
+                    data[i] = inChar;
+                    ++i;
+                }
+                
+            } while (inChar != '\r');
+
+            //translate data
+            return (Convert.ToInt16(new string(data, 4, 2), 16) * 256 + Convert.ToInt16(new string(data, 6, 2), 16)) / 4;
+        }
+
+        void OBDInit()
+        {
+            //string for response from OBD chip
+            string response;
+
+            //close the port if it is open
+            if (serialOBD.IsOpen)
+                serialOBD.Close();            
+
+            /* ------------------------ Set the port parameters ---------------------- */
+
+            //use a delegate to access the GUI combobox across threads
+            this.Invoke(new MethodInvoker(delegate() { serialOBD.PortName = cmbPorts.SelectedItem.ToString(); }));
+
+            serialOBD.BaudRate = 9600;
+            serialOBD.DataBits = 8;
+            serialOBD.Parity = Parity.None;
+            serialOBD.StopBits = StopBits.One;
+
+            //open the port
+            serialOBD.Open();
+            this.Invoke(new MethodInvoker(delegate() { rtbPrgStat.Text += "Serial port opened.\n"; }));
+
+            /* ----------------------- Initialize OBD Communication ---------------------- */
+
+            //reset the OBD chip
+            serialOBD.Write("ATZ\r\n");
+
+            //wait for the OBD chip to reset
+            Thread.Sleep(1000);
+
+            //set baud rate to 115200
+            serialOBD.Write("ATBRD 23\r\n");
+            Thread.Sleep(200);
+
+            serialOBD.Close();
+            serialOBD.BaudRate = 115200;
+            serialOBD.Open();
+            serialOBD.Write("\r");
+
+            Thread.Sleep(1000);
+
+            this.Invoke(new MethodInvoker(delegate() { rtbPrgStat.Text += "Baud rate set.\n"; }));
+
+            //ask the OBD chip to find a protocol (can send any command)
+            serialOBD.Write("0100\r\n");
+
+            //wait for the OBD chip to find a protocol
+            Thread.Sleep(2000);
+
+            //clean out the buffers
+            serialOBD.DiscardInBuffer();
+            serialOBD.DiscardOutBuffer();
+
+            //make sure the chip is working
+            do
+            {
+                this.Invoke(new MethodInvoker(delegate() { rtbPrgStat.Text += "Waiting for IGN response.\n"; }));
+                serialOBD.Write("ATIGN\r\n");
+                serialOBD.ReadTo("\r");
+                response = serialOBD.ReadTo("\r");
+                Thread.Sleep(100);
+            } while (response != "ON");
+
+            //diable echos
+            serialOBD.Write("ATE0\r\n");
+
+            this.Invoke(new MethodInvoker(delegate() { rtbPrgStat.Text += "OBD Chip reset and ready.\n"; }));
+            this.Invoke(new MethodInvoker(delegate() { btnComm.Enabled = true; }));
+            this.Invoke(new MethodInvoker(delegate() { btnInit.Enabled = false; }));
+            //new Thread(OBDComm2).Start();
+        }
+
+        void OBDComm2()
+        {
+            int rpm = 0, oldrpm = 0;
+            while (serialOBD.IsOpen)
+            {
+                oldrpm = rpm;
+                rpm = GetRPM();
+                UpdateTextBox(txtRPM, rpm.ToString());
+                this.Invoke(new MethodInvoker(delegate() { barRPM.Value = rpm * 100 / 8000; }));
+
+                //UpdateTextBox(txtSpeed, GetSpeed().ToString());
+            }
+        }
+
+    }
+}
