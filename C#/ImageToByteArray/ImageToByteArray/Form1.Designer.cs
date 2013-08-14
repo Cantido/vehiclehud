b00cee4a6c60965f901e6bcd221758259e9f74de
catacon <ventscanner5@yahoo.com>
2013-08-13 22:53:03 -0500

    Initial add of Image to Byte Array project.
    This C# program will convert a monochrome bitmap to a byte array.  The
    byte array is in the correct format for the Noritake 128x64 VFD.

---
+++ C#/ImageToByteArray/ImageToByteArray/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ImageToByteArray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileBtn = new System.Windows.Forms.Button();
+            this.pbInput = new System.Windows.Forms.PictureBox();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileBtn
+            // 
+            this.fileBtn.Location = new System.Drawing.Point(199, 36);
+            this.fileBtn.Name = "fileBtn";
+            this.fileBtn.Size = new System.Drawing.Size(110, 23);
+            this.fileBtn.TabIndex = 0;
+            this.fileBtn.Text = "Select Image";
+            this.fileBtn.UseVisualStyleBackColor = true;
+            this.fileBtn.Click += new System.EventHandler(this.fileBtn_Click);
+            // 
+            // pbInput
+            // 
+            this.pbInput.Location = new System.Drawing.Point(190, 99);
+            this.pbInput.Name = "pbInput";
+            this.pbInput.Size = new System.Drawing.Size(128, 64);
+            this.pbInput.TabIndex = 2;
+            this.pbInput.TabStop = false;
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbOutput.Location = new System.Drawing.Point(13, 218);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(494, 148);
+            this.rtbOutput.TabIndex = 3;
+            this.rtbOutput.Text = "";
+            this.rtbOutput.WordWrap = false;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(404, 189);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 4;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 378);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.pbInput);
+            this.Controls.Add(this.fileBtn);
+            this.Name = "Form1";
+            this.Text = "Image to Byte Array";
+            ((System.ComponentModel.ISupportInitialize)(this.pbInput)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button fileBtn;
+        private System.Windows.Forms.PictureBox pbInput;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.Button btnCopy;
+    }
+}
+
---
+++ C#/ImageToByteArray/ImageToByteArray/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ImageToByteArray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fileBtn_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the open file dialog box.
+            OpenFileDialog openFile = new OpenFileDialog();
+
+            // Set filter options and filter index.
+            openFile.Filter = "Bitmap Files (.bmp)|*.bmp";
+            openFile.FilterIndex = 1;
+
+            openFile.Multiselect = false;
+
+            // Call the ShowDialog method to show the dialog box.
+            DialogResult userClickedOK = openFile.ShowDialog();
+
+            // Process input if the user clicked OK.
+            if (userClickedOK == DialogResult.OK)
+            {
+                rtbOutput.Clear();
+                using (FileStream stream = new FileStream(openFile.FileName, FileMode.Open, FileAccess.Read))
+                {
+                    
+                    Bitmap bm = (Bitmap)Image.FromFile(openFile.FileName);
+                    pbInput.Image = bm;
+                    Color pixel;
+
+                    Byte[] byteArray = new Byte[bm.Width*(bm.Height)/8];
+
+                    Byte tempByte = 0;
+
+                    for (int t = 0; t < bm.Height / 8; ++t)
+                    {
+                        for (int x = 0; x < bm.Width; ++x)
+                        {
+                            tempByte = 0;
+                            for (int y = 0; y < 8; ++y)
+                            {
+                                pixel = bm.GetPixel(x, (8 * t) + y);
+
+                                if (pixel.R < 128 && pixel.G < 128 && pixel.B < 128)
+                                    tempByte |= (byte)(1 << y);                                    
+                            }
+
+                            byteArray[x + bm.Width * t] = tempByte;
+                        }
+                    }
+
+                    int z = 0;
+                    foreach (var item in byteArray)
+                    {
+                        rtbOutput.Text += ("0x" + item.ToString("X2"));
+                        ++z;
+                        rtbOutput.Text += ((z % bm.Width == 0) ? "\n" : ", ");
+                    }
+                }
+            }
+            
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (rtbOutput.Text != "")
+                    System.Windows.Forms.Clipboard.SetText(rtbOutput.Text);
+            }
+            catch (System.Runtime.InteropServices.ExternalException) {}
+        }
+    }
+}
