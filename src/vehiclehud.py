7a06c3940aa3b66d211d5f14368504728071c4b3
Robert Richter <robert.c.richter@gmail.com>
2013-09-17 21:57:08 +0100

    Refactor device commands and improve IO speed

--- src/vehiclehud.py
+++ src/vehiclehud.py
@@ -10,10 +10,20 @@ class ELM327:
         elif sys.platform == "linux2":
             port = "/dev/ttyUSB0"
             
-        self.ser = serial.Serial(port,baudrate=38400, timeout=3)
+        self.ser = serial.Serial(port,baudrate=38400, timeout=1)
+        self.sio = io.TextIOWrapper(io.BufferedRWPair(self.ser, self.ser), encoding="ASCII", newline="\r", line_buffering=True)
 
-        #self.write("ATZ")
-        #self.write("ATSP0")
+        self.disable_space_printing()
+        self.disable_command_echo()
+
+    def isOpen(self):
+        return ser.isOpen()
+    
+    def write(self, text):
+        self.ser.write(text + "\r")
+
+    def readline(self):
+        return self.sio.readline()
         
     def write(self, text):
         self.ser.write(text + "\r")
@@ -32,13 +42,56 @@ class ELM327:
         self.write(request)
         return self.readlines()
 
-    def getRPM(self):
-        data = self.data_request("010c")
+    def disable_space_printing(self):
+        self.write("ATS0")
 
-        if data == "":
-            return 0
-        else:
-            return int(data, 16) / 4
+    def enable_space_printing(self):
+        self.write("ATS1")
+
+    def disable_command_echo(self):
+        self.write("ATE0")
+
+    def enable_command_echo(self):
+        self.write("ATE1")
+
+    def reset(self):
+        self.write("ATZ")
+
+    def auto_find_protocol(self):
+        self.write("ATSP00")
+    
+    def get_data(self, request):
+        """
+        Request vehicle data by PID
+
+        This should return ONLY the bytes representing the data requested.
+        It should be possible to parse an integer straight from the data
+        this method returns, if the given PID requests an integer value.
+        """
+        request_mode = int(request[0:2], 16)
+        request_pid = int(request[2:4], 16)
+        
+        self.write(request)
+        data = device.readlines()[2]
+        if "UNABLE TO CONNECT" in data:
+            raise Exception("Unable to connect to the vehicle")
+
+        response_mode = int(data[0:2], 16)
+        response_pid = int(data[2:4], 16)
+        response_payload = data[4:]
+
+        assert(request_mode == response_mode - 0x40)
+        assert(request_pid == response_pid)
+        
+        return response_payload
+        
+    def get_engine_rpm(self):
+        data = self.data_request("010c")
+        return int(data, 16) / 4
+
+    def get_vehicle_speed(self):
+        data = self.data_request("010d")
+        return int(data, 16)
 
 if __name__ == "__main__":
     device = ELM327()
