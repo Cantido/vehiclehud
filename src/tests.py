470be0ff1556e24adf7622a3e13a7ac19893efc2
Robert Richter <robert.c.richter@gmail.com>
2013-09-17 23:41:27 +0100

    Create simple test for data requests

--- src/tests.py
+++ src/tests.py
@@ -8,11 +8,32 @@ import unittest
 
 import vehiclehud
 
+class MockSerial:
+    speed_str = "55" # 0x05
+    expected_return = ""
+    
+    def readline(self):
+        return ">echoed command \r empty line \r{}\r".format(MockSerial.expected_return)
+
+    def write(self, text):
+        if text.lower() == "010d\r":
+            MockSerial.expected_return = str("410D" + MockSerial.speed_str) 
+        
 class TestInitialize(unittest.TestCase):
     def runTest(self):
         device = vehiclehud.ELM327()
         with self.assertRaises(serial.SerialException):
             device.connect()
+
+class TestOBDRequest(unittest.TestCase):
+    def setUp(self):
+        self.device = vehiclehud.ELM327()
+        self.device.ser = MockSerial()
+        self.device.sio = MockSerial()
+        
+    def runTest(self):
+        speed = self.device.get_vehicle_speed()
+        self.assertEquals(speed, int(MockSerial.speed_str, 16))
 
 if __name__ == "__main__":
     unittest.main(exit=False)
