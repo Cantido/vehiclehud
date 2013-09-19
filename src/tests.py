bcf025fa841c20d6642779c43eca7f3d19a1426a
Robert Richter <robert.c.richter@gmail.com>
2013-09-19 15:14:58 -0500

    Allow users to set expected mock response

--- src/tests.py
+++ src/tests.py
@@ -9,30 +9,22 @@ import unittest
 import vehiclehud
 
 class MockDevice(vehiclehud.ELM327):
-    speed_str = "55" # 0x05
-    expected_return = ""
-
     def connect(self):
         pass
-
     def config(self):
         pass
-    
     def isOpen(self):
         return True
-
     def assert_connected(self):
         pass
-    
     def readline(self):
-        return "{}\r\r>".format(MockDevice.expected_return)
-
+        return "{}\r\r>".format(self.expected_response)
     def readlines(self):
         return self.readline().split("\r")
-
     def write(self, text):
-        if text.lower() == "010d":
-            MockDevice.expected_return = str("410D" + MockDevice.speed_str) 
+        pass
+    def set_expected_data(self, expected):
+        self.expected_response = expected
         
 class TestInitialize(unittest.TestCase):
     def test_connect(self):
@@ -45,13 +37,21 @@ class TestInitialize(unittest.TestCase):
         with self.assertRaises(vehiclehud.VehicleHUDException):
             device.config()
 
-class TestOBDRequest(unittest.TestCase):
+class TestGetSpeed(unittest.TestCase):
     def setUp(self):
         self.device = MockDevice()
-        
-    def runTest(self):
+        self.expected_speed = 85
+        self.hex_speed = hex(self.expected_speed)[2:]
+
+    def test_valid_device_response(self):
+        self.device.set_expected_data("410d" + self.hex_speed)
         speed = self.device.get_vehicle_speed()
-        self.assertEquals(speed, int(MockDevice.speed_str, 16))
+        self.assertEquals(speed, self.expected_speed)
+    
+    def test_invalid_device_response(self):
+        self.device.set_expected_data("410c" + self.hex_speed)
+        with self.assertRaises(vehiclehud.VehicleHUDException):
+            speed = self.device.get_vehicle_speed()
 
 if __name__ == "__main__":
     unittest.main(exit=False)
